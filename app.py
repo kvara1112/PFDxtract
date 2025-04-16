795310c61cf434fefb63fb521e5114d50734bdbb
Dr Georgina Cosma <g.cosma@lboro.ac.uk>
2025-04-16 06:41:02 +0100

    Update app.py
    Working version. Needs error checking for the graphs and the filtering to allow a selection of areas, and more filtering.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pyLDAvis 
 import pyLDAvis.sklearn 
-import pandas as pd
+import pandas as pd 
 import numpy as np
 from datetime import datetime, timedelta
 import re
