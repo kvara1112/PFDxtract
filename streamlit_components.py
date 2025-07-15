62a6a21087140a04ec0e383ec25851e8b9c95096
kvara1112 <151623098+kvara1112@users.noreply.github.com>
2025-07-15 12:46:55 +0100

    fixing

--- streamlit_components.py
+++ streamlit_components.py
@@ -1686,9 +1686,9 @@ def render_theme_analysis_dashboard(data: pd.DataFrame = None):
                         outlinewidth=1
                     ),
                     hoverongaps=False,
-                    textx=count_pivot.values,  # This will show the count in the hover
-                
-                    hovertemplate='Year: %{x}<br>Theme: %{y}<br>Percentage: %{z}%<br>Count: %{textx}<extra></extra>'
+                    text="hello",#count_pivot.values,  # This will show the count in the hover
+
+                    hovertemplate='Year: %{x}<br>Theme: %{y}<br>Percentage: %{z}%<br>Count: %{text}<extra></extra>'
                 )
                 
                 fig.add_trace(heatmap)
