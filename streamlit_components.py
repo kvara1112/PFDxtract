7e09fa06351f544897f7b080c2c0c8a67b0ff691
kvara1112 <kvara2003@hotmail.com>
2025-07-17 10:54:31 +0100

    removed print

--- streamlit_components.py
+++ streamlit_components.py
@@ -2341,7 +2341,7 @@ def render_theme_analysis_dashboard(data: pd.DataFrame = None):
         theme_display_map2 = {theme: improved_truncate_text(theme, max_length=100) for theme in top_themes}
         # Format column and index labels
         formatted_themes = [theme_display_map[theme] for theme in top_theme_corr.columns]
-        print(formatted_themes)
+        
         # Create the correlation matrix visualization
         fig_corr_matrix = px.imshow(
             top_theme_corr,
