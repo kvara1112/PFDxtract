1a0679d00e57cbae424403be00064858654e2f2f
kvara1112 <151623098+kvara1112@users.noreply.github.com>
2025-07-23 11:07:01 +0100

    markdown

--- modules/streamlit_components.py
+++ modules/streamlit_components.py
@@ -408,7 +408,10 @@ def upload_PFD_reports():
                 st.session_state.current_data = None
                 st.session_state.processed = False
                 st.success("Cleared all uploaded reports.")
-                st.markdown("")  
+                st.session_state.uploaded_reports_files = []  # Ensure session is cleared
+                # Hide the markdown
+                st.experimental_rerun()  # Rerun the app to clear the UI
+                  
         with col2:
             if st.button("Process uploaded reports"):
                 if len(st.session_state.uploaded_reports_files) < 5:
