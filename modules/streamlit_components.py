169779665c3306af0bf783567f8ae3cca347d0e3
kvara1112 <151623098+kvara1112@users.noreply.github.com>
2025-08-27 10:21:17 +0100

    display filename

--- modules/streamlit_components.py
+++ modules/streamlit_components.py
@@ -892,7 +892,7 @@ def upload_reports(is_PFD):
     elif state["retry_files"] and report_key == "Other":
         st.warning(f"{len(retry_needed)} files failed to process. Please retry.")
         for r in retry_needed:
-            st.write(r["Subject"])
+            st.write(r["Filename"])
 
     # Final processed data
     if state["processed"] and state["current_data"] is not None:
