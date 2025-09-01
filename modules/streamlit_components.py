e2cd01577a382007298cb192f2f5febf1f57dd55
kvara1112 <151623098+kvara1112@users.noreply.github.com>
2025-09-01 11:47:41 +0100

    uploaded pfd current data only for pfd

--- modules/streamlit_components.py
+++ modules/streamlit_components.py
@@ -916,10 +916,11 @@ def upload_reports(is_PFD):
             st.dataframe(state["current_data"])
             if report_key == "PFD":
                 show_export_options(state["current_data"], prefix="uploaded")
+                st.session_state.current_data = state["current_data"]
+                st.session_state.data_source = 'uploaded PFD'
             elif report_key == "Other":
                 show_export_options(state["current_data"], prefix="uploaded_other")
-            st.session_state.current_data = state["current_data"]
-            st.session_state.data_source = 'uploaded PFD'
+            
         else:
             st.markdown("No data could be processed")
         st.markdown("### Begin a New Upload")
