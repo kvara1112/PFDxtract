b03f85cc6efb75fbb1062d1ac513e140af59d6fb
kvara1112 <151623098+kvara1112@users.noreply.github.com>
2026-01-23 15:13:25 +0000

    dp

--- modules/streamlit_components.py
+++ modules/streamlit_components.py
@@ -2451,7 +2451,7 @@ def render_evaluations_tab(isPFD: bool):
 
                 cbar = ax.collections[0].colorbar
                 cbar.ax.set_ylabel("Proportion of predictions", rotation=-90, labelpad=25, color='white')
-                cbar.ax.set_yticklabels([f"{t*100:0f}%" for t in cbar.get_ticks()], color='white')  # keep 2dp
+                cbar.ax.set_yticklabels([f"{t*100:1f}%" for t in cbar.get_ticks()], color='white')  # keep 2dp
 
                 cbar.ax.text(0.5, 1.05, 'Always Predicted', ha='center', va='bottom', color='white', transform=cbar.ax.transAxes)
                 cbar.ax.text(0.5, -0.1, 'Not Predicted', ha='center', va='top', color='white', transform=cbar.ax.transAxes)
