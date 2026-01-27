923cc8f06ea088ff29b4cb6f1746f12eac8755e0
kvara1112 <151623098+kvara1112@users.noreply.github.com>
2026-01-27 13:03:38 +0000

    checking

--- modules/streamlit_components.py
+++ modules/streamlit_components.py
@@ -2995,7 +2995,7 @@ def render_theme_analysis_dashboard(isPFD: bool, data: pd.DataFrame = None):
                 active_filters.append(f"Year: {selected_years[0]}")
             else:
                 active_filters.append(f"Years: {selected_years[0]}-{selected_years[1]}")
-        print("after selection", filtered_df["Theme"])
+
         if isPFD:
             if "coroner_area" not in missing_recommended and "coroner_name" not in missing_recommended is None:
                 if area_filter_type == "Select Specific Areas" and selected_areas:
